247e0276e3ee69aa85f8f969e220194eac63159b
DrivableJonatan <38322093+DrivableJonatan@users.noreply.github.com>
2018-05-02 19:18:47 +0200

    refresh
    en uppdatering, helt enkelt

--- Resources/templates/header.php
+++ Resources/templates/header.php
@@ -1,6 +1,8 @@
 <DOCTYPE html>
 <html lang="sv">
 	<head>
+		<meta charset="utf-8">
 		<link rel="stylesheet" href="resources/stylesheets/menu.css"/>
 		<link rel="stylesheet" href="resources/stylesheets/footer.css"/>
 		<link rel="stylesheet" href="resources/stylesheets/content.css"/>
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans"> <!--applicera med "font-family: 'Open Sans', sans-serif;" i CSS :)-->
--- index.php
+++ index.php
@@ -1,11 +1,37 @@
 <?php
+
 require("resources/templates/header.php");
+
 ?>
+
 <title>Välkommen till Choko-LAD!</title>
+
+</head>
+<body>
+
+	<div id="content">
+
+<div class="wrapper">
+  <div class="grid">
+    <div class="sidebar">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur rerum, nulla ducimus deleniti veritatis. Autem placeat molestiae id maxime hic quia mollitia molestias vem. Perferendis reiciendis ex tempora, odio iure quibusdam ut consequuntur minima Porro consectetur rerum ipsum nulla aspernatur facilis eaque quis iste accusantium, ducimus ad incidunt quibusdam, numquam qui odit voluptates necessitatibus repudiandae hic veritatis, facere atque, corrupti expedita animi. Perferendis, omnis.Sint, ut minima, animi distinctio recusandae cumque! Veritatis ab eius, ad repudiandae eum dolorem sit.</p>
+    </div>
+    <div class="content">
+     <img src="resources/images/choco_lad.png" alt="logotype">
+	 </div>
+    <div class="ad">
+      This is an ad
+    </div>
+    <div class="sidebar">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non, libero obcaecati dolore officia suscipit ex, deserunt enim sit eligendi ipsam, quia corporis cumque tempora aliquid debitis perspiciatis, facilis numquam quas!</p></div>
+    <div class="content">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos cumque iste necessitatibus quasi, sapiente eos quod eligendi inventore eius nisi culpa error suscipit vel alias nostrum provident assumenda. Inventore, eum.</p>
+    </div>
+  </div>
+</div>
+
 <?php
-require("resources/templates/menu.php");
-?>
-Mumsiga bilder på grejer
-<?php
+
 require("resources/templates/footer.php");
+
 ?>
